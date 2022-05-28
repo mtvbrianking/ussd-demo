7498c8f2f42975d720df3de6792ece247b9d1891
mtvbrianking <mtvbrianking@gmail.com>
2022-05-28 22:19:51 +0300

    Remove sparors ussd

--- app/Http/Controllers/Api/UssdController.php
+++ app/Http/Controllers/Api/UssdController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Ussd\CheckUserAction;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Sparors\Ussd\Facades\Ussd;
 
 class UssdController extends Controller
 {
@@ -17,37 +15,11 @@ class UssdController extends Controller
 
     public function __invoke(Request $request): Response
     {
-        $request['text'] = $this->lastInput($request->text);
-        $request['phoneNumber'] = preg_replace('/[^0-9]/', '', $request->phoneNumber);
-
-        $ussdMachine = Ussd::machine()
-            ->setFromRequest([
-                'sessionId' => 'sessionId',
-                'phoneNumber' => 'phoneNumber',
-                'network' => 'networkCode',
-                'serviceCode' => 'serviceCode',
-                'input' => 'text',
-            ])
-            ->setInitialState(CheckUserAction::class)
-            ->setResponse(function (string $message, string $action) {
-                return $message;
-            });
-
-        return response($ussdMachine->run());
+        return response('Testing...');
     }
 
-    protected function lastInput(?string $input) : ?string
+    public function africastalking(Request $request): Response
     {
-        if(! $input) {
-            return $input;
-        }
-
-        if(! preg_match('/^[\d+\*]+[\d+]$/', $input)) {
-            return $input;
-        }
-
-        $inputs = explode('*', $input);
-
-        return end($inputs);
+        return response('Testing...');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\Api\UssdController;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/', [UssdController::class, '__invoke']);
+Route::any('/ussd/', [UssdController::class, '__invoke']);
+
+Route::any('/ussd/at', [UssdController::class, 'africastalking']);
