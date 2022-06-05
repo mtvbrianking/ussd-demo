56e70d426ce66b04a275e94998e1b1dc5d3a8a63
mtvbrianking <mtvbrianking@gmail.com>
2022-06-05 20:05:19 +0300

    Fix syntax error

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,12 @@
 use App\Http\Controllers\Api\UssdController;
 use Illuminate\Support\Facades\Route;
 
-Route::::match(['get', 'post'], '/ussd', [UssdController::class, '__invoke']);
+Route::match(['get', 'post'], '/ussd', [UssdController::class, '__invoke']);
 
-Route::::match(['get', 'post'], '/ussd/africastalking', [UssdController::class, 'africastalking']);
+Route::match(['get', 'post'], '/ussd/africastalking', [UssdController::class, 'africastalking']);
 
-Route::::match(['get', 'post'], '/ussd/korba', [UssdController::class, 'korba']);
+Route::match(['get', 'post'], '/ussd/korba', [UssdController::class, 'korba']);
 
-Route::::match(['get', 'post'], '/ussd/nalo', [UssdController::class, 'nalo']);
+Route::match(['get', 'post'], '/ussd/nalo', [UssdController::class, 'nalo']);
 
-Route::::match(['get', 'post'], '/ussd/nsano', [UssdController::class, 'nsano']);
+Route::match(['get', 'post'], '/ussd/nsano', [UssdController::class, 'nsano']);
